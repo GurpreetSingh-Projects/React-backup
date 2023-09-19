4f8337d7e26b01bafa6ed75cb1de0ffeca76a6c2
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-09-19 16:56:36 +0530

    added 1 and 2 folders

---
+++ 2/index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(
+  <ul>
+    <li>Banana</li>
+    <li>Apple</li>
+    <li>Orange</li>
+    <li>Pineapple</li>
+    <li>Guava</li>
+    <li>Leechi</li>
+  </ul>,
+  document.getElementById("root")
+);
