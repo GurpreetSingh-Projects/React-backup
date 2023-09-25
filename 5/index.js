f1d24c9282f089fb90e303737eb39d320775ede8
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-09-25 16:58:09 +0530

    code update

---
+++ 5/index.js
@@ -0,0 +1,15 @@
+const page = (
+  <div>
+    <h1> Eatables</h1>
+    <ul>
+      <li>Snacks</li>
+      <li>Fruits</li>
+      <li>Vegetables</li>
+      <li>Milk Products</li>
+      <li>Meat based meals</li>
+    </ul>
+    <p>These can further be divided into multiple categories</p>
+    <p>React practice</p>
+  </div>
+);
+ReactDOM.render(page, document.getElementById("root"));
---
+++ 6/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
+ReactDOM.render(<p>Hello Everyone!!</p>, document.getElementsByClassName("App"))
+
