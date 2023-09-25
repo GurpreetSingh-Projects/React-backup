bd6fce4bf2d90026456df4897268ab963ce66a01
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-09-26 03:17:40 +0530

    added another lab files

---
+++ reactlab 2/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading name=' Bob' />
+            <Heading name=' John'/>
+        </div>
+    );
+};
+
+export default App;
