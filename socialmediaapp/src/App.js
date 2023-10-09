fe42f3f18fb6db3eeaadd6bbbe6025de696ecd6f
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-10-10 03:11:29 +0530

    code update

--- socialmediaapp/src/App.js
+++ socialmediaapp/src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/Topbar/Header.js";
+import Header from "./components/Topbar/Header.jsx";
 import Home from "./pages/home/Home.js";
 import HomeIcon from "@mui/icons-material/Home";
 
--- socialmediaapp/src/components/Topbar/Header.jsx
+++ socialmediaapp/src/components/Topbar/Header.jsx
@@ -1,14 +1,17 @@
 import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
 
 export default function Header() {
   return (
-    <div className="header_container">
-      <div className="header_left">
-        <span className="span">Social Media</span>
+    <div className="headercontainer">
+      <div className="headerleft">
+        <span className="logo">Social Media</span>
       </div>
-      <div className="header_mid">
+      <div className="headermid">
         <div className="searchbar">
           <SearchIcon />
           <input
@@ -18,15 +21,27 @@ export default function Header() {
           ></input>
         </div>
       </div>
-      <div className="header_right">
+      <div className="headerright">
         <div>
           <div className="headerlink">Homepage</div>
           <div className="headerlink">Timeline</div>
         </div>
         <div className="headerIcons">
-          
+          <div className="headericonitem"></div>
+          <PersonIcon />
+          <div className="headericonbadge">1</div>
+
+          <div className="headericonitem"></div>
+          <ChatIcon/>
+          <div className="headericonbadge">2</div>
+
+          <div className="headericonitem"></div>
+          <NotificationsIcon/>
+          <div className="headericonbadge">3</div>
         </div>
+        <img src="assets/person/1.jpeg" alt="" className="headerimage" />
       </div>
+      
     </div>
   );
 }
