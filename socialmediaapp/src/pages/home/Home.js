257292e2023cef47222fca6912b607a7848ab9ab
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-10-10 21:12:23 +0530

    code update

--- socialmediaapp/src/pages/home/Home.js
+++ socialmediaapp/src/pages/home/Home.js
@@ -1,7 +1,14 @@
 import Header from "../../components/Topbar/Header";
-
+import sidebar from "../../components/Sidebar/sidebar";
+import feed from "../../components/Feed/feed";
+import rightbar from "../../components/Rightbar/rightbar";
 export default function Home() {
   return (
-    <Header/>
-  )
+    <>
+      <Header />
+      <sidebar/>
+      <feed />
+      <rightbar/>
+    </>
+  );
 }
