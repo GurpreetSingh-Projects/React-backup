8cb8e86e657bbf5cafdb725dc8a62fec1e4943e3
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-10-20 02:35:22 +0530

    added form1

---
+++ Folder/form1/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Heading from "./Heading"
+
+function App() {
+  const [word, setWord] = useState("Eat");
+
+  const clickHandler = () => {
+    setWord("Drinkkk");
+  }
+  return (
+    <div>
+      <Heading message={word + " at Little Lemon Restraunt"} />
+      <button onClick={clickHandler}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
