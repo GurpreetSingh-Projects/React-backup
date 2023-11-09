4f8772ffe017c5b3701bbe740be97ff72707a1ea
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-09 18:36:16 +0530

    bug fixes

--- socialmediav1/src/components/Shared/Shared.jsx
+++ socialmediav1/src/components/Shared/Shared.jsx
@@ -5,28 +5,42 @@ export default function Shared() {
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
+          <img
+            className="shareProfileImg"
+            src="/assets/person/1.jpeg"
+            alt="profile pic"
+          />
           <input className="shareInput" placeholder="What's on Your Mind ?" />
         </div>
         <hr className="shareHr" />
         <div className="shareBottom">
           <div className="shareOptions">
-            <FontAwesomeIcon icon="fa-solid fa-clapperboard"  className="shareIcon" />
+            <FontAwesomeIcon
+              icon="fa-solid fa-clapperboard"
+              className="shareIcon"
+            />
+            <span className="shareOptionText">Photo or Video</span>
+          </div>
+
+          <div className="shareOptions">
+            <FontAwesomeIcon
+              icon="fa-solid fa-clapperboard"
+              className="shareIcon"
+            />
             <span className="shareOptionText">Photo or Video</span>
           </div>
           <div className="shareOptions">
-            <FontAwesomeIcon icon="fa-solid fa-clapperboard"  className="shareIcon" />
+            <FontAwesomeIcon
+              icon="fa-solid fa-clapperboard"
+              className="shareIcon"
+            />
             <span className="shareOptionText">Photo or Video</span>
           </div>
           <div className="shareOptions">
-            <FontAwesomeIcon icon="fa-solid fa-clapperboard"  className="shareIcon" />
-            <span className="shareOptionText">Photo or Video</span>
-          </div>
-          <div className="shareOptions">
-            <FontAwesomeIcon icon="fa-solid fa-clapperboard"  className="shareIcon" />
-            <span className="shareOptionText">Photo or Video</span>
-          </div>
-          <div className="shareOptions">
-            <FontAwesomeIcon icon="fa-solid fa-clapperboard"  className="shareIcon" />
+            <FontAwesomeIcon
+              icon="fa-solid fa-clapperboard"
+              className="shareIcon"
+            />
             <span className="shareOptionText">Photo or Video</span>
           </div>
         </div>
